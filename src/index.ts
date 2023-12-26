dbdbf3aa51cb4e5fc27910e236f8724b6a6d39a4
Okem Onyeka <jesse007onyeka@gmail.com>
2023-12-26 21:48:31 +0100

    Minor updates

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import { connectDB } from './config/db';
-import dotenv from 'dotenv/config';
+import dotenv from 'dotenv';
 
-// dotenv.config();
+dotenv.config();
 connectDB();
 const port = process.env.PORT ?? 3000;
 
